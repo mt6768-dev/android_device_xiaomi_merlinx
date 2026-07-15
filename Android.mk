c6ce7acaf220c8db2c5cd9b6d49a6295b67743ae
Matsvei Niaverau <matvej2002@gmail.com>
2026-07-15 11:32:49 +0200

    merlinx: Move init.project.rc target to soong
    Change-Id: I0d2172c19acc11d9394571d6d55fa1f4a3685fb9

--- Android.mk
+++ Android.mk
@@ -8,13 +8,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter merlinx, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-include $(CLEAR_VARS)
-LOCAL_MODULE	   := init.project.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.project.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
 endif
